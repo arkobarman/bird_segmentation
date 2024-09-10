0c0ceebb557ef79d321354f75061bc6dbc98ce7a
Alanjy Huang <alanhuang@Alanjys-MacBook-Air.local>
2024-09-10 00:33:01 -0500

    Finish Draw.py BarGraph and Random ID test

---
+++ Draw.py
@@ -0,0 +1,44 @@
+from main import *
+import random
+import matplotlib.pyplot as plt
+
+def draw_histogram(pairs,  title="Relation between size and species", xlabel="Category_Id", ylabel="Bird size (pixel)"):
+    # Create the histogram
+    id, frequencies = zip(*pairs)
+    plt.bar(id,frequencies, edgecolor='black')
+    
+    # Add titles and labels
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    # Show the plot
+    plt.show()
+def draw_histogram_with_text(pairs,  title="Relation between size and species", xlabel="Category_Id", ylabel="Bird size (pixel)"):
+    # Create the histogram
+    id, frequencies = list(pairs.keys()),list(pairs.values())
+    print(id)
+    plt.bar(id,frequencies, edgecolor='black')
+    for id_number,average in pairs.items():
+        plt.text(id_number-0.5, int(average)+20, str(round(average,2)), fontsize=8, color='red') 
+    # Add titles and labels
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    # Show the plot
+    plt.show()
+if __name__=='__main__':
+    graph_data=[]
+    data=json_loader("CoCoTest.json")
+ #   for i in data["annotations"]:
+       # graph_data.append(i['category_id'],count_area(i["segmentation"][0]))
+ #       graph_data.append((random.randint(1,20),count_area(i["segmentation"][0])))
+    averge_pair=[]
+    for i in data["annotations"]:
+        id=int(random.randint(0,20))
+        averge_pair.append([id,count_area(i["segmentation"][0])])
+        graph_data.append([id,count_area(i["segmentation"][0])])
+    average_map=count_average(averge_pair)
+    draw_histogram(graph_data)
+    draw_histogram_with_text(average_map)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import random
+from Draw import *
 def json_loader(filename):
     # Read the JSON file
     with open(filename, 'r') as file:
@@ -10,12 +12,36 @@ def count_area(vertices):
     area = 0
 
     # Shoelace formula
-    for i in range(n):
-        x1, y1 = vertices[i]
-        x2, y2 = vertices[(i + 1) % n]  # Next vertex (wraps around to the first)
+    for i in range(0,n,2):
+        x1, y1 = vertices[i],vertices[i+1]
+        x2, y2 = vertices[(i + 2) % n],vertices[(i + 2) % n+1]  # Next vertex (wraps around to the first)
         area += x1 * y2 - y1 * x2
     return abs(area) / 2
-
+def count_average(pair):
+    total={}
+    numbers={}
+    output={}
+    for ID_area_pair in pair:
+        if ID_area_pair[0] in total:
+            total[ID_area_pair[0]]+=ID_area_pair[1]
+            numbers[ID_area_pair[0]]+=1
+        else:
+            numbers[ID_area_pair[0]]=1
+            total[ID_area_pair[0]]=ID_area_pair[1]
+        for key,value in total.items():
+            output[key]=value/numbers[key]
+    return output
 if __name__=='__main__':
-    json_loader("CoCoTest.json")
-    count_area()
+    data=json_loader("CoCoTest.json") #loadin data
+    averge_pair=[]
+    graph_data=[]
+    for i in data["annotations"]:
+        id=int(random.randint(0,20))
+        averge_pair.append([id,count_area(i["segmentation"][0])])
+        graph_data.append([id,count_area(i["segmentation"][0])])
+        #print(i['category_id'],count_area(i["segmentation"][0]))
+    average_map=count_average(averge_pair)
+    for i,j in average_map.items():
+        print(i,j)
+    draw_histogram(graph_data)
+    draw_histogram_with_text(average_map)
